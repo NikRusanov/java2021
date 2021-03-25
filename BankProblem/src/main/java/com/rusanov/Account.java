2da69d3eb5062f6630ba456cc89f03a2490de2b6
Nikita Rusanov <kenya.rusanov@gmail.com>
2021-03-25 09:46:59 +0300

    Fix. Blocking objects in method

--- BankProblem/src/main/java/com/rusanov/Account.java
+++ BankProblem/src/main/java/com/rusanov/Account.java
@@ -1,13 +1,13 @@
 package com.rusanov;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Account {
-    private String id;
+    private final String id;
     private Integer balance;
-
+    private final Lock lock = new ReentrantLock();
+    private static final int TIME_TO_SLEEP = 200;
     public Account(String id, Integer balance){
         this.id = id;
         this.balance = balance;
@@ -19,4 +19,32 @@ public class Account {
     public void decrement(int summ) {this.balance -= summ;}
     public void increment(int summ) {this.balance += summ;}
 
+
+    // Если первый аккаунт не используется, блокируем и проверяем второй аккаунт.
+    // Если и со вторым все нормально, блокируем и его, делаем перевод и выходим из цикла
+    // В finally блоках снимаем блокировки
+    // Если же второй занят, то снимаем блокировку первого и ждем, затем повторяем
+    public void transfer(Account toAcc, int amount) throws InterruptedException {
+        while (true) {
+            if(this.lock.tryLock()) {
+                try {
+                    if(toAcc.lock.tryLock()) {
+                        try {
+                            if(amount > balance) {
+                                throw new IllegalArgumentException("Not enough money on " + id);
+                            }
+                            toAcc.increment(amount);
+                            this.decrement(amount);
+                            break;
+                        } finally {
+                            toAcc.lock.unlock();
+                        }
+                    }
+                } finally {
+                    this.lock.unlock();
+                }
+                Thread.sleep(TIME_TO_SLEEP);
+            }
+        }
+    }
 }
--- BankProblem/src/test/java/com/rusanov/TransferTest.java
+++ BankProblem/src/test/java/com/rusanov/TransferTest.java
@@ -12,7 +12,7 @@ public class TransferTest {
         Account account2 = new Account("2", 0);
         Account account3 = new Account("3", 500);
         Transfer transfer = new Transfer();
-        Thread thread1 = new Thread(() -> { transfer.transferMoney(account1, account2, 500); });
+        Thread thread1 = new Thread(() -> transfer.transferMoney(account1, account2, 500));
         Thread thread2 = new Thread(() -> transfer.transferMoney(account2, account1, 250));
 
         //account 1:1000
@@ -21,7 +21,7 @@ public class TransferTest {
         //
         thread1.start();
         thread2.start();
-
+        Thread.sleep(1000);
         System.out.println("account 1:" + account1.getBalance());
         System.out.println("account 2:" + account2.getBalance());
         System.out.println("account 3:" + account3.getBalance());
